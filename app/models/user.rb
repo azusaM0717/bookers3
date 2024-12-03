10d1aafe35946abffdc561b93e73eb6e29d500cd
AzusaMorishita <184392623+azusaM0717@users.noreply.github.com>
2024-12-03 02:15:54 +0000

    goodbutton

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,20 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :validatable
+  
   has_one_attached :profile_image
   has_many :books, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true, length: { in: 2..20 }
   validates :introduction, length: { maximum: 50 }
 
-  devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :validatable
 
   def get_profile_image(width, height)
     if profile_image.attached?
-      profile_image.variant(resize_to_limit: [width, height]).processed
+      profile_image.variant(resize: "#{width}x#{height}")
     else
-      file_path = Rails.root.join('app/assets/images/sample-user1.jpg')
-      self.profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-      profile_image.variant(resize_to_limit: [width, height]).processed
+      'no_image.jpg'
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "home/about", to: "homes#about"
-  resources :books, only: [:show, :create, :index, :edit, :update, :destroy]
-  resources :users, only: [:new, :show, :create, :index, :edit, :update]
-
+  resources :books, only: [:show, :create, :index, :edit, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :index, :edit, :update]
 end
