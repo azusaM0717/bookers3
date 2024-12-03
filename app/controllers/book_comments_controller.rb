68876ac3cc348fd99b0dc962b721214b85f50a35
AzusaMorishita <184392623+azusaM0717@users.noreply.github.com>
2024-12-03 13:29:24 +0000

    [finish]comment

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,12 +5,12 @@ def create
   comment = current_user.book_comments.new(book_comment_params)
   comment.book_id = book.id
   comment.save
-  redirect_to request.refere
+  redirect_to request.referer
 end
 
 def destroy
-  book = BookComment.find(params[:id])
-  comment.destroy
+  BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+  redirect_to request.referer
 end
 
 private
