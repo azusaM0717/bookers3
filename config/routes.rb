2ca774a94e6b9021c6d78a632410267234709044
AzusaMorishita <184392623+azusaM0717@users.noreply.github.com>
2024-12-03 02:26:51 +0000

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get "home/about", to: "homes#about"
   resources :books, only: [:show, :create, :index, :edit, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :index, :edit, :update]
 end
