79d7c605b9c845e7e09bf9428188942fd740dbda
AzusaMorishita <184392623+azusaM0717@users.noreply.github.com>
2024-12-04 03:17:38 +0000

    [finish]search

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "home/about", to: "homes#about"
-  get "/search", to; "searches#search"
+  get "/search", to: "searches#search"
 
   resources :books, only: [:show, :create, :index, :edit, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  
+
   resources :users, only: [:show, :index, :edit, :update] do
     resource :relationships, only: [:create, :destroy]
       get "followings" => "relationships#followings", as: "followings"
