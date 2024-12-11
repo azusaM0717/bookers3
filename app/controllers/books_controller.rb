e82d11b7a47fbf660d851e5ed2b7a49814326d81
AzusaMorishita <184392623+azusaM0717@users.noreply.github.com>
2024-12-11 00:48:45 +0000

    [finish]7a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,7 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+  
   def show
     @book = Book.find(params[:id])
     @user = @book.user
@@ -42,7 +45,12 @@ class BooksController < ApplicationController
 
 
   def index
-    @books = Book.all
+    to = Time.current.at_end_of_day
+    from = (to - 6.day).at_beginning_of_day
+    @books = Book.all.sort {|a,b|
+      b.favorites.where(created_at: from...to).size <=>
+      a.favorites.where(created_at: from...to).size
+    }
     @book = Book.new
     @user = current_user
   end
